8e81950604704030d376979fe23bfb125fc0b52a
iampenkin@gmail.com <54068102+penkin-repo@users.noreply.github.com>
2026-01-14 23:59:41 +0300

    deploy github pages / upload ver 0.15

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-// import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://penkindesign.ru',
   integrations: [
     react(),
     tailwind({
